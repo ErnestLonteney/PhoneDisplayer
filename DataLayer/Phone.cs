8f66a4a0531c95d5bda8d4542376003215caee11
Volodymyr N <volodymyr@mail.com>
2024-01-19 19:35:08 +0100

    Added new lines

--- DataLayer/Phone.cs
+++ DataLayer/Phone.cs
@@ -3,9 +3,13 @@
     public class Phone
     {
         public int Id { get; set; }
+        
         public string Name { get; set; }
+
         public int Price { get; set; }
+
         public int CompanyId { get; set; }
+
         public virtual Company Company { get; set; }
     }
 }
